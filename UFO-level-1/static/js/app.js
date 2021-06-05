f23b88ade31c66f1c1de7a09ddf63f1c92ee68ba
Morgaine B <morgainebowers@gmail.com>
2021-06-05 18:59:10 -0400

    Got the table set up again

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,3 +4,18 @@ console.log(tableData);
 
 // YOUR CODE HERE!
 // Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Loop Through UFO Sighting data
+tableData.forEach(function(sighting) {
+    console.log(sighting);
+    // Append each UFO Sighting object to a row
+    var row = tbody.append("tr");
+    // Use Object.entries to console.log each UFO Sighting object value
+    Object.entries(sighting).forEach(function([key, value]) {
+        console.log(key, value);
+        // Append a cell per UFO Sighting value
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
