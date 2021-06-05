134ef9adbfed13828d787e9f685c6933d12c2a4c
Morgaine B <morgainebowers@gmail.com>
2021-06-05 17:44:58 -0400

    Looped through UFO sightings and appended tr

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,3 +5,10 @@ console.log(tableData);
 // YOUR CODE HERE!
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+
+// Loop through the data and console.log each UFO sighting object
+data.forEach(function(UFOReport) {
+    console.log(UFOReport);
+    //Append one table row 'tr' for each UFO sighting object
+    var row = tbody.append("tr")
+});
