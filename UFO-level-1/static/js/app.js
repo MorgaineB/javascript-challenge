17a1772c833bfd0efd05b2a602ff6fa9461c3469
Morgaine B <morgainebowers@gmail.com>
2021-06-05 19:18:03 -0400

    Added comments to explain each step

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -22,23 +22,32 @@ tableData.forEach(function(ufoSighting) {
 
 //Select the button
 var button = d3.select("#filter-btn");
+//Select the form
 var form = d3.select("#form")
+//Create event handlers
 button.on("click", runEnter);
 form.on ("submit", runEnter);
 
+//Complete the event handler function for the form
 function runEnter() {
 
+    //Prevent the page from refreshing
     d3.event.preventDefault();
 
+    //Clear out the table
     tbody.html("");
 
+    //Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
+    //Get the value property of the input element
     var inputValue = inputElement.property("value");
     console.log(inputValue);
 
+    //Filter the data
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
     console.log(filteredData);
 
+    //Add the filtered data only to the table
     filteredData.forEach(function(selections) {
         console.log(selections);
         // Append to the table like above
